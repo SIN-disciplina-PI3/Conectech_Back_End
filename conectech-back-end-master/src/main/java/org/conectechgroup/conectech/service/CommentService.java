0f9111cc92e06aa98f933f505565631a252b3724
Laranunes1 <110771720+Laranunes1@users.noreply.github.com>
2024-06-04 15:18:46 -0300

    Create CommentService.java

---
+++ conectech-back-end-master/src/main/java/org/conectechgroup/conectech/service/CommentService.java
@@ -0,0 +1,63 @@
+package org.conectechgroup.conectech.service;
+
+import org.conectechgroup.conectech.model.Comment;
+import org.conectechgroup.conectech.DTO.CommentDTO;
+import org.conectechgroup.conectech.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public Comment findById(String id){
+        return commentRepository.findById(id).orElse(null);
+    }
+
+    public List<Comment> findAll(){
+        return commentRepository.findAll();
+    }
+
+    public List<Comment> findByPostId(String postId){
+        return commentRepository.findByPostId(postId);
+    }
+
+    public Comment insert(Comment comment){
+        return commentRepository.insert(comment);
+    }
+
+    public void delete(String id){
+        commentRepository.deleteById(id);
+    }
+
+    public Comment update(Comment comment){
+        return commentRepository.save(comment);
+    }
+    public void deleteComment(String id) {
+        commentRepository.deleteById(id);
+    }
+    public Comment updateComment(String id, Comment comment) {
+        comment.setId(id);
+        return commentRepository.save(comment);
+    }
+    public Comment save(Comment comment){
+        return commentRepository.save(comment);
+    }
+    public List<Comment> findByAuthorId(String authorId){
+        return commentRepository.findByAuthorId(authorId);
+    }
+
+    //convert to DTO
+    public CommentDTO convertToDTO(Comment comment){
+        CommentDTO dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setContent(comment.getContent());
+        dto.setAuthorName(comment.getAuthor().getName());
+        return dto;
+    }
+
+}
