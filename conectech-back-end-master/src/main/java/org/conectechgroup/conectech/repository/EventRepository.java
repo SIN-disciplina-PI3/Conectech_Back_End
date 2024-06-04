0416f7fb7751cc26c54f2ee46668c4ad75e15c86
felipeoliveira25 <119983478+felipeoliveira25@users.noreply.github.com>
2024-06-04 14:28:10 -0300

    Create EventRepository.java

---
+++ conectech-back-end-master/src/main/java/org/conectechgroup/conectech/repository/EventRepository.java
@@ -0,0 +1,10 @@
+package org.conectechgroup.conectech.repository;
+
+import org.conectechgroup.conectech.model.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+@org.springframework.stereotype.Repository
+public interface EventRepository extends MongoRepository<Event, String> {
+
+    Event findByTitle(String title);
+}
